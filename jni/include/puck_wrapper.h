12f53ef599157315ab22c8970a2100659bd9b4d2
Cao Ky Hieu <32695157+caokyhieu@users.noreply.github.com>
2025-10-17 17:32:19 +0100

    Add QueryIndex function to puck_wrapper.h

--- jni/include/puck_wrapper.h
+++ jni/include/puck_wrapper.h
@@ -81,6 +81,9 @@ int knn_query_index(jlong indexPointer, jfloat* queryVector, jint dimension, jin
  */
 void knn_free_index(jlong indexPointer);
 
+jobjectArray QueryIndex(knn_jni::JNIUtilInterface * jniUtil, JNIEnv * env, jlong indexPointerJ,
+                                jfloatArray queryVectorJ, jint kJ, jobject methodParamsJ);
+
 // ========== DEPRECATED / STUB METHODS ==========
 // These throw exceptions - use the above methods instead
 
